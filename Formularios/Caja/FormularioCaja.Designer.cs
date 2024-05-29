dc02adb0569e1441e84e81e2c572c38d50ac443e
Matias Novillo <novillo.matias1@gmail.com>
2024-05-29 00:32:13 -0300

    Se avanzo con una demo de caja para mostrarle a Juan

--- Formularios/Caja/FormularioCaja.Designer.cs
+++ Formularios/Caja/FormularioCaja.Designer.cs
@@ -37,8 +37,21 @@
             menuItemMain = new ToolStripMenuItem();
             btnGuardar = new Button();
             label1 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label4 = new Label();
+            comboBox2 = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            label5 = new Label();
+            comboBox3 = new ComboBox();
+            label6 = new Label();
             statusStrip1.SuspendLayout();
             toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // lblTitulo
@@ -56,7 +69,7 @@
             // 
             statusStrip1.ImageScalingSize = new Size(20, 20);
             statusStrip1.Items.AddRange(new ToolStripItem[] { statusLabel });
-            statusStrip1.Location = new Point(0, 467);
+            statusStrip1.Location = new Point(0, 542);
             statusStrip1.Name = "statusStrip1";
             statusStrip1.Size = new Size(821, 26);
             statusStrip1.TabIndex = 1;
@@ -102,7 +115,7 @@
             btnGuardar.BackColor = Color.Black;
             btnGuardar.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnGuardar.ForeColor = SystemColors.Window;
-            btnGuardar.Location = new Point(629, 381);
+            btnGuardar.Location = new Point(629, 450);
             btnGuardar.Name = "btnGuardar";
             btnGuardar.Size = new Size(180, 78);
             btnGuardar.TabIndex = 11;
@@ -115,18 +128,152 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F);
             label1.ForeColor = SystemColors.Window;
-            label1.Location = new Point(12, 431);
+            label1.Location = new Point(12, 500);
             label1.Name = "label1";
             label1.Size = new Size(127, 28);
             label1.TabIndex = 24;
             label1.Text = "(*) Requerido";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 179);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(301, 27);
+            dateTimePicker1.TabIndex = 25;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(278, 28);
+            label2.TabIndex = 26;
+            label2.Text = "Fecha y hora de transacción (*)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 320);
+            label3.Name = "label3";
+            label3.Size = new Size(139, 28);
+            label3.TabIndex = 27;
+            label3.Text = "Descripción (*)";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Cobranza", "Proveedores", "Gastos", "Retiros" });
+            comboBox1.Location = new Point(12, 351);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(301, 28);
+            comboBox1.TabIndex = 28;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 10.8F);
+            radioButton1.ForeColor = Color.White;
+            radioButton1.Location = new Point(12, 265);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(93, 29);
+            radioButton1.TabIndex = 29;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Entrada";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 10.8F);
+            radioButton2.ForeColor = Color.White;
+            radioButton2.Location = new Point(135, 265);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(80, 29);
+            radioButton2.TabIndex = 30;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Salida";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(12, 234);
+            label4.Name = "label4";
+            label4.Size = new Size(207, 28);
+            label4.TabIndex = 31;
+            label4.Text = "Tipo de transacción (*)";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Juan Cruz", "Lanza", "Trade Food", "Comarcas ganaderas", "Comida", "Ingresos brutos", "Sueldos" });
+            comboBox2.Location = new Point(12, 385);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(301, 28);
+            comboBox2.TabIndex = 32;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(401, 182);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(278, 27);
+            numericUpDown1.TabIndex = 33;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(401, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(96, 28);
+            label5.TabIndex = 34;
+            label5.Text = "Dinero (*)";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Cheque", "Banco", "Efectivo" });
+            comboBox3.Location = new Point(401, 265);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(301, 28);
+            comboBox3.TabIndex = 36;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(401, 234);
+            label6.Name = "label6";
+            label6.Size = new Size(146, 28);
+            label6.TabIndex = 35;
+            label6.Text = "Forma de pago";
             // 
             // FormularioCaja
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(821, 493);
+            ClientSize = new Size(821, 568);
+            Controls.Add(comboBox3);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(numericUpDown1);
+            Controls.Add(comboBox2);
+            Controls.Add(label4);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker1);
             Controls.Add(label1);
             Controls.Add(btnGuardar);
             Controls.Add(toolStrip1);
@@ -141,6 +288,7 @@
             statusStrip1.PerformLayout();
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -155,5 +303,17 @@
         private Button btnGuardar;
         private ToolStripMenuItem menuItemMain;
         private Label label1;
+        private DateTimePicker dateTimePicker1;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label4;
+        private ComboBox comboBox2;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private ComboBox comboBox3;
+        private Label label6;
     }
 }
