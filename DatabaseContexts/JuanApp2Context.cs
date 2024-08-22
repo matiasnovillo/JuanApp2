58205259513793f00f7e63bd9fc5f42f800d3ed2
Matias Novillo <novillo.matias1@gmail.com>
2024-08-22 19:46:49 -0300

    Se dejo como debe estar el JuanApp2Context.cs, respecto a las ConnectionStrings

--- DatabaseContexts/JuanApp2Context.cs
+++ DatabaseContexts/JuanApp2Context.cs
@@ -57,24 +57,14 @@ namespace JuanApp2.DatabaseContexts
             {
                 string ConnectionString = "";
 #if DEBUG
-                string IP = "192.168.28.14";
-                string Server = "sql4.baehost.com";
-
-                ConnectionString = "Password=1vb&10tD5;" +
-                    "Persist Security Info=True;" +
-                    "User ID=fiyista1_JuanApp2Admin;" +
-                    "Initial Catalog=fiyista1_JuanApp2;" +
-                    "Data Source=sql4.baehost.com;" +
-                    "TrustServerCertificate=True";
-
-                //ConnectionString = "data source =.; " +
-                //   "initial catalog = JuanApp2; " +
-                //   "Integrated Security = SSPI;" +
-                //   " MultipleActiveResultSets=True;" +
-                //   "Pooling=false;" +
-                //   "Persist Security Info=True;" +
-                //   "App=EntityFramework;" +
-                //   "TrustServerCertificate=True;";
+                ConnectionString = "data source =.; " +
+                   "initial catalog = JuanApp2; " +
+                   "Integrated Security = SSPI;" +
+                   " MultipleActiveResultSets=True;" +
+                   "Pooling=false;" +
+                   "Persist Security Info=True;" +
+                   "App=EntityFramework;" +
+                   "TrustServerCertificate=True;";
 #else
                 string IP = "192.168.28.14";
                 string Server = "sql4.baehost.com";
