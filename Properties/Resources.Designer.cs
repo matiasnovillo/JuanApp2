434e71afa04c2abffdb42c4f74cba0a8f9eef18f
Matias Novillo <novillo.matias1@gmail.com>
2024-08-23 12:25:07 -0300

    Se actualizo el Acerca De tomando como pivote el de JuanApp1. Tambien se hizo un acomodamiento de recursos (imagenes) dispuesto ahora en un carpeta para tal fin

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace JuanApp2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoDark1 {
+            get {
+                object obj = ResourceManager.GetObject("LogoDark1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
